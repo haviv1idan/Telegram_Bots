634371fea839422678d991aa4378ee867f298180
Idan Haviv <haviv1idan@gmail.com>
2023-08-11 23:49:24 +0300

    add User and Message tables

--- New_Grocery_Bot/project/app/admin.py
+++ New_Grocery_Bot/project/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Shop, OnlineShop
+from .models import Product, Shop, OnlineShop, User, Message
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -15,8 +15,18 @@ class OnlineShopAdmin(admin.ModelAdmin):
     list_display = ('chain', 'name', 'website', 'sale', 'price', 'barcode')
     list_filter = ('chain', 'name', 'website', 'sale', 'price', 'barcode')
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'first_name', 'last_name', 'language_code')
+    list_filter = ('id', 'name', 'first_name', 'last_name', 'language_code') 
+    
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'unix_time', 'is_bot', 'user_id')
+    list_filter = ('id', 'text', 'unix_time', 'is_bot', 'user_id')
+
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Shop, ShopAdmin)
 admin.site.register(OnlineShop, OnlineShopAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Message, MessageAdmin)
